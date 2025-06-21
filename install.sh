a398845edc66125e0b7215cb0b8d5c76c3623f4f
kudos <riky.wolf@outlook.it>
2025-06-21 16:03:21 +0200

    v0.1.14 - Added volman to thunar install - Removed old code

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ if [[ "$reply" =~ ^([Yy]|)$ ]]; then
   if pacman -Qs dolphin &>/dev/null; then
     sudo pacman -Rns --noconfirm dolphin
   fi
-  sudo pacman -Sy --needed --noconfirm thunar
+  sudo pacman -Sy --needed --noconfirm thunar gvfs thunar-volman
   echo "✓ Dolphin replaced with Thunar."
 else
   echo "Skipped replacing dolphin."
@@ -75,40 +75,4 @@ if [[ -d "$runs_dir" ]]; then
   done
 fi
 
-# --- Main logic (to be modularized in future) ---
-#
-#if [[ -s "$current_dir/packages/pacman.txt" ]]; then
-#  sudo pacman -Sy --needed --noconfirm - < "$current_dir/packages/pacman.txt"
-#fi
-#
-#if ! command -v yay &>/dev/null; then
-#  git clone https://aur.archlinux.org/yay.git /tmp/yay
-#  (cd /tmp/yay && makepkg -si --noconfirm)
-#fi
-#if [[ -s "$current_dir/packages/aur.txt" ]]; then
-#  yay -S --needed --noconfirm - < "$current_dir/packages/aur.txt"
-#fi
-#
-#echo "✓ All packages installed."
-#
-###
-### STOW DOTFILES
-###
-#for dir in "$HOME/.config/hypr" "$HOME/.config/waybar" "$HOME/.config/rofi" "$HOME/.config/nwg-bar" "/usr/share/sddm/themes/catppuccin-macchiato"; do
-#  if [[ -d "$dir" ]]; then
-#    mkdir -p "$HOME/.config/old_configs"
-#    sudo mv "$dir" "$HOME/.config/old_configs/"
-#    mkdir -p "$dir"
-#  fi
-#  mkdir -p "$dir"
-#done
-#
-#stow --verbose --restow --dir=home/.config --target="$HOME/.config/hypr" hypr
-#stow --verbose --restow --dir=home/.config --target="$HOME/.config/waybar" waybar
-#stow --verbose --restow --dir=home/.config --target="$HOME/.config/rofi" rofi
-#stow --verbose --restow --dir=home/.config --target="$HOME/.config/nwg-bar" nwg-bar
-#stow --verbose --restow --dir=home/.config --target="$HOME/.config/qt6ct" qt6ct
-#
-#sudo stow --verbose --restow --target=/usr/share/sddm/themes/catppuccin-macchiato catppuccin-macchiato
-#
 echo "✓ All configurations deployed."
